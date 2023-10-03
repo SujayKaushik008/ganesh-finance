4496be77879f75c31987816b3bb910f3c9c6a8ec
M SAI NIVEDH KUMAR <M SAI NIVEDH KUMAR@10.120.174.134>
2023-10-03 14:41:07 +0530

    updated loan_type--nivedh

---
+++ src/main/java/com/oracle/finance/entity/LoanApplication.java
@@ -0,0 +1,6 @@
+package com.oracle.finance.entity;
+
+public class LoanApplication {
+	private String loan_application_number,clerk_id,customer_id,loan_tenure,application_date;
+	private int loan_type,requested_amount,application_status;
+}
