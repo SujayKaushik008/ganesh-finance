fcc06e66d53b4c940f8f70420d097722d38545cb
SujayKaushik008 <sujay.kaushik@oracle.com>
2023-10-03 12:20:53 +0530

    first commit

---
+++ src/test/java/com/oracle/finance/GaneshFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaneshFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
