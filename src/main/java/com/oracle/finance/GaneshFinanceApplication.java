714ffa0715d673af845aa597ee79681f0e45077d
SujayKaushik008 <sujay.kaushik@oracle.com>
2023-10-03 12:29:00 +0530

    initial commit

--- src/main/java/com/oracle/finance/GaneshFinanceApplication.java
+++ src/main/java/com/oracle/finance/GaneshFinanceApplication.java
@@ -8,6 +8,7 @@ public class GaneshFinanceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GaneshFinanceApplication.class, args);
+		System.out.println("first commit");
 	}
 
 }
