1770b3751e78765936f5470398746f35422f6f3d
sainmara <sai.nivedh.kumar.maram@oracle.com>
2023-10-08 11:59:22 +0530

    STRATED TESTING

---
+++ src/main/java/com/oracle/finance/tests/AuthenticationServiceTester.java
@@ -0,0 +1,15 @@
+//package com.oracle.finance.tests;
+////import org.junit.Test;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//import static org.mockito.Mockito.when;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//
+//public class AuthenticationServiceTester {
+//	
+//}
